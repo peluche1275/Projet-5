17c23eddb3e13028cac11e7b8c55af37fd474e52
Nathan <nathan.altomare.belier@gmail.com>
2020-04-02 16:23:17 +0200

    Start of the project

---
+++ Web/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+// DEFINITION OF THE DEFAULT APPLICATION //
+
+const DEFAULT_APP = 'Frontend';
+ 
+if (!isset($_GET['app']) || !file_exists(__DIR__.'/../App/'.$_GET['app'])) $_GET['app'] = DEFAULT_APP;
+
+// CLASS LOADS //
+ 
+require __DIR__.'/../lib/Framework/SplClassLoader.php';
+ 
+// $OCFramLoader = new SplClassLoader('OCFram', __DIR__.'/../lib');
+// $OCFramLoader->register();
+ 
+// $appLoader = new SplClassLoader('App', __DIR__.'/..');
+// $appLoader->register();
+ 
+// $modelLoader = new SplClassLoader('Model', __DIR__.'/../lib/vendors');
+// $modelLoader->register();
+ 
+// $entityLoader = new SplClassLoader('Entity', __DIR__.'/../lib/vendors');
+// $entityLoader->register();
+ 
+// $formBuilderLoader = new SplClassLoader('FormBuilder', __DIR__.'/../lib/vendors');
+// $formBuilderLoader->register();
+ 
+// APPLICATION LAUNCH //
+
+$appClass = 'App\\'.$_GET['app'].'\\'.$_GET['app'].'Application';
+$app = new $appClass;
+$app->run();
---
+++ lib/Framework/SplClassLoader.php
@@ -0,0 +1,83 @@
+<?php
+
+class SplClassLoader
+{
+    // PROPERTIES //
+
+    private 
+    $_fileExtension = '.php',
+    $_namespace,
+    $_includePath,
+    $_namespaceSeparator = '\\';
+
+    // CONSTRUCTOR //
+
+    public function __construct($namespace = null, $includePath = null)
+    {
+        $this->_namespace = $namespace;
+        $this->_includePath = $includePath;
+    }
+
+    // METHODS //
+
+    public function register()
+    {
+        spl_autoload_register(array($this,'loadClass'));
+    }
+
+    public function unregister()
+    {
+        spl_autoload_unregister(array($this,'loadClass'));
+    }
+
+    public function LoadClass($className)
+    {
+        if (null === $this->_namespace || $this->_namespace.$this->_namespaceSeparator === substr($className, 0, strlen($this->_namespace.$this->_namespaceSeparator))) 
+        {
+            $fileName = '';
+            $namespace = '';
+            if (false !== ($lastNsPos = strripos($className, $this->_namespaceSeparator))) 
+            {
+              $namespace = substr($className, 0, $lastNsPos);
+              $className = substr($className, $lastNsPos + 1);
+              $fileName = str_replace($this->_namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            }
+            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->_fileExtension;
+            require ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+        }
+    }
+
+    // GETTERS //
+    
+    public function getNamespaceSeparator()
+    {
+        return $this->_namespaceSeparator;
+    }
+
+    public function getIncludePath()
+    {
+        return $this->_includePath;
+    }
+
+    public function getFileExtension()
+    {
+        return $this->_fileExtention;
+    }
+
+    // SETTERS //
+
+    public function setNamespaceSeparator($separator)
+    {
+        $this->_namespaceSeparator = $separator;
+    }
+
+    public function setIncludePath($includePath)
+    {
+        $this->_includePath = $includePath;
+    }
+
+    public function setFileExtention($fileExtension)
+    {
+        $this->_fileExtention = $fileExtension;
+    }
+}
