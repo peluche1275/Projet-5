8be0762cca2e9a69de50bae601de3cefab25a583
Nathan <nathan.altomare.belier@gmail.com>
2020-04-03 10:38:31 +0200

    First Design

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -8,26 +8,40 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!-- <link rel="stylesheet" href="/assets/css/main.css" type="text/css" />
-  <link rel="icon" href="/images/favicon.ico" /> -->
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 
 </head>
 
 <body>
   <div id="wrapper">
+
     <header id="header">
-      <h1><a href="/">Le Super jeue</a></h1>
-      <nav class="links">
+      <div class="logo">
+        <span class="icon fa-gem"></span>
+      </div>
+      <div class="content">
+        <div class="inner">
+          <h1><a href="/">Le Super jeu</a></h1>
+          <p>Le super jeu trop bien qui a l'air trop cool</p>
+        </div>
+      </div>
+      <nav>
         <ul>
-        <li><a href="/">Accueil</a></li>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/login">Connexion</a></li>
+          <li><a href="/inscription">Inscription</a></li>
+          <li><a href="/contact">Contact</a></li>
           <?php if ($user->isAuthenticated()) { ?>
-            
+
           <?php } ?>
         </ul>
       </nav>
     </header>
 
     <div id="main">
+
       <section>
         <?php if ($user->hasFlash()) echo '<p style="text-align: center;">', $user->getFlash(), '</p>'; ?>
 
@@ -35,7 +49,13 @@
       </section>
     </div>
 
-    <footer></footer>
+    <footer id="footer">
+      <p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+    </footer>
   </div>
+  <!-- BG -->
+  <div id="bg"></div>
+
 </body>
+
 </html>
